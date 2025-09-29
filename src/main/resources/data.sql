bb04379100fcec5cd7a454b071dfd0a888d39f1a
pinkkila <petteri.pinkkila@outlook.com>
2025-09-29 17:55:28 +0300

    add stories card

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,8 @@ delete
 from story;
 
 insert into story (id, story_title, story_text)
-values (99, 'Example', 'This is example story');
+values (99, 'Example', 'This is an example story.'),
+       (100, 'Longer Example', 'This is a longer example story with more words in it. How does it flow. Does it go to the another line?');
 
 
 SELECT setval('story_id_seq', (SELECT MAX(id) FROM story));
