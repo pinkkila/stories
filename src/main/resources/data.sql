6446d693ec35d7678b182a312affa1398f8b0835
pinkkila <petteri.pinkkila@outlook.com>
2025-09-30 22:51:55 +0300

    add styles to the story card

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ from story;
 
 insert into story (id, story_title, story_text)
 values (99, 'Example', 'This is an example story.'),
-       (100, 'Longer Example', 'This is a longer example story with more words in it. How does it flow. Does it go to the another line?');
+       (100, 'Another Example', 'This is an another example story.');
 
 
 SELECT setval('story_id_seq', (SELECT MAX(id) FROM story));
